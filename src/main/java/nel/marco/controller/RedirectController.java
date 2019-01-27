91a96c6a4be88d89cd08d9cb6052df7b30b1a2d8
Marco-PC\Marco <epossmurfmarco@gmail.com>
2019-01-27 10:18:00 +0200

    extracting the zip src and updating source code

---
+++ src/main/java/nel/marco/controller/RedirectController.java
@@ -0,0 +1,17 @@
+package nel.marco.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@RestController
+public class RedirectController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public void redirect(HttpServletResponse response) throws IOException {
+        response.sendRedirect("/helloWorldFlow");
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/mainPage.jsp
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <title>Hello world Example</title>
+    </head>
+    <body>
+        <div>
+            Hellos
+        </div>
+    </body>
+</html>
+<script>
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+});
+</script>
